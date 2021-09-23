8b959116a81598ce3ed7b698eff4ac7593f69be9
Abriele Qudsi <abrielebromozentia@gmail.com>
2021-09-23 13:00:25 +0100

    feat(be): add login mutation resolvers

--- server/graphql/resolvers/index.resolver.js
+++ server/graphql/resolvers/index.resolver.js
@@ -1,6 +1,29 @@
+// const usersResolver = require('./user.resolver')
+// const programsResolver = require('./program.resolver')
+// const modulesResolver = require('./module.resolver')
+const User = require('../../models/user.model')
+const Program = require('../../models/program.model')
+const Module = require('../../models/module.model')
+
 const resolvers = {
-    Query: {
-        sayHi: () => 'Hello world!'
+    Mutation: {
+        login: async (_, { username, password }, context, info) => {
+            // Validate user data
+
+            const user = await User.findOne({ username });
+
+            // Handle username
+            if (!user) {
+                console.log("User not found")
+            }
+
+            // Handle password
+            if (password != user.password) {
+                console.log("Wrong credentials")
+            }
+            // Return User Info
+            return user
+        }
     }
 }
 
