60f400a1c29b18c35945dc5c51197417e47b806a
Abriele Qudsi <abrielebromozentia@gmail.com>
2021-09-24 13:55:42 +0100

    fix(gql-be): add userProgram as output to login.resolver

--- server/graphql/resolvers/user.resolver.js
+++ server/graphql/resolvers/user.resolver.js
@@ -1,31 +1,37 @@
 const User = require('../../models/user.model');
+const Program = require('../../models/program.model');
 
 module.exports = {
     Query : {
         getClassmates: async (_, { programCode }) => {
             // fetch classmates
-            const classmates = await User.find({ program: programCode })
+            const classmates = await User.find({ programCodes: programCode })
             return classmates
-
         }
     },
     Mutation: {
         login: async (_, { email, password }, context, info) => {
-            // Validate user data
-
+            
+            // Handle user data
             const user = await User.findOne({ email });
-
-            // Handle username
             if (!user) {
                 throw new Error("User not found")
             }
-
-            // Handle password
             if (password != user.password) {
                 throw new Error("Wrong credentials")
             }
+            // Handle program data
+            const programInfo = await Program.find({ 
+                programCode:  {
+                    $in: user.programCodes
+                }
+            })
+
             // Return User Info
-            return user
+            return {
+                userInfo: user,
+                userProgram: programInfo
+            }
         }
     }
 }
