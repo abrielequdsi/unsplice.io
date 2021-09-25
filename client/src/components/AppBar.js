a12644efcc14d344743d03b6bb8cb76e2bad64c7
Abriele Qudsi <abrielebromozentia@gmail.com>
2021-09-26 00:29:38 +0100

    fix(dashboard-fe): fix AppBar & Drawer

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -3,26 +3,41 @@ import MuiAppBar from '@mui/material/AppBar';
 
 const drawerWidth = 240;
 
-const AppBar = () => {
-    return (
-        styled(MuiAppBar, {
-            shouldForwardProp: (prop) => prop !== 'open',
-        })(({ theme, open }) => ({
-            zIndex: theme.zIndex.drawer + 1,
-            transition: theme.transitions.create(['width', 'margin'], {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.leavingScreen,
-            }),
-            ...(open && {
-                marginLeft: drawerWidth,
-                width: `calc(100% - ${drawerWidth}px)`,
-                transition: theme.transitions.create(['width', 'margin'], {
-                    easing: theme.transitions.easing.sharp,
-                    duration: theme.transitions.duration.enteringScreen,
-                }),
-            }),
-        }))
-    );
-}
+const AppBar = styled(MuiAppBar, { shouldForwardProp: (prop) => prop !== 'open', })(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+})
+)
 
 export default AppBar;
+
+// import Link from '@mui/material/Link';
+// import Typography from '@mui/material/Typography';
+
+// const Copyright = (props) => {
+//     return (
+//         <Typography variant="body2" color="text.secondary" align="center" {...props}>
+//             {'Copyright © '}
+//             <Link color="inherit" href="https://github.com/abrielequdsi/unsplice.io">
+//                 unsplice.io
+//             </Link>{' '}
+//             {new Date().getFullYear()}
+//             {'.'}
+//         </Typography>
+//     );
+// }
+
+// export default Copyright;
+
+
