2ed6838b9367d8c0f97f24ed7854116691e6909d
Abriele Qudsi <abrielebromozentia@gmail.com>
2021-09-26 00:33:17 +0100

    fix(dashboard-fe): modularise AppBar & Drawer

--- client/src/components/mui-styled/appBar.js
+++ client/src/components/mui-styled/appBar.js
@@ -22,22 +22,4 @@ const AppBar = styled(MuiAppBar, { shouldForwardProp: (prop) => prop !== 'open',
 
 export default AppBar;
 
-// import Link from '@mui/material/Link';
-// import Typography from '@mui/material/Typography';
-
-// const Copyright = (props) => {
-//     return (
-//         <Typography variant="body2" color="text.secondary" align="center" {...props}>
-//             {'Copyright © '}
-//             <Link color="inherit" href="https://github.com/abrielequdsi/unsplice.io">
-//                 unsplice.io
-//             </Link>{' '}
-//             {new Date().getFullYear()}
-//             {'.'}
-//         </Typography>
-//     );
-// }
-
-// export default Copyright;
-
 
