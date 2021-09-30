6cf9e23dc10eb9c722f1c0eec2612a77ac3fce6a
n-lundie <noah.lundie@gmail.com>
2021-09-30 12:46:58 +0100

    file: Drawer.js line: 10 change: M to lowercase m

--- client/src/components/Drawer.js
+++ client/src/components/Drawer.js
@@ -7,62 +7,62 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import { styled } from '@mui/material/styles';
 import MuiDrawer from '@mui/material/Drawer';
 // Components
-import MainListItems from './list-item/MainList';
+import MainListItems from './list-item/mainList';
 import SecondaryListItems from './list-item/SecondList';
-import ProgramChange from './ProgramChange.js'
+import ProgramChange from './ProgramChange.js';
 
 const drawerWidth = 240;
 
-const StyledDrawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-        '& .MuiDrawer-paper': {
-            position: 'relative',
-            whiteSpace: 'nowrap',
-            width: drawerWidth,
-            transition: theme.transitions.create('width', {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.enteringScreen,
-            }),
-            boxSizing: 'border-box',
-            ...(!open && {
-                overflowX: 'hidden',
-                transition: theme.transitions.create('width', {
-                    easing: theme.transitions.easing.sharp,
-                    duration: theme.transitions.duration.leavingScreen,
-                }),
-                width: theme.spacing(7),
-                [theme.breakpoints.up('sm')]: {
-                    width: theme.spacing(9),
-                },
-            }),
-        },
+const StyledDrawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  '& .MuiDrawer-paper': {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
     }),
-)
+    boxSizing: 'border-box',
+    ...(!open && {
+      overflowX: 'hidden',
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up('sm')]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
 
 const Drawer = ({ toggleDrawer, open }) => {
-    return (
-        <StyledDrawer variant="permanent" open={open}>
-            <Toolbar
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'flex-end',
-                    px: [1],
-                }}
-            >
-                <Box>
-                    <ProgramChange />
-                </Box>
-                <IconButton onClick={toggleDrawer}>
-                    <ChevronLeftIcon />
-                </IconButton>
-            </Toolbar>
-            <Divider />
-            <MainListItems />
-            <Divider />
-            <SecondaryListItems />
-        </StyledDrawer>
-    )
-}
+  return (
+    <StyledDrawer variant="permanent" open={open}>
+      <Toolbar
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+          px: [1],
+        }}
+      >
+        <Box>
+          <ProgramChange />
+        </Box>
+        <IconButton onClick={toggleDrawer}>
+          <ChevronLeftIcon />
+        </IconButton>
+      </Toolbar>
+      <Divider />
+      <MainListItems />
+      <Divider />
+      <SecondaryListItems />
+    </StyledDrawer>
+  );
+};
 
 export default Drawer;
