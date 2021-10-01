e12c2dade06de664553d5dea67a2452817776c9e
Kirstin Buchanan <buchananmkirstin@gmail.com>
2021-10-01 15:31:39 +0100

    test: overview card renders without crashing

--- client/src/__test__/OverviewCard.test.js
+++ client/src/__test__/OverviewCard.test.js
@@ -3,22 +3,42 @@ import { expect, test } from '@jest/globals';
 import { render } from '@testing-library/react';
 import OverviewCard from '../components/cards/OverviewCard';
 
-test('overview card should show a title', async () => {
-  const module = {
-    name: 'Test Title',
-    // desc: 'Test Description',
-    // moduleCode: 'TEST123',
-    // progress: 50,
-  };
-  const overviewCard = render(
-    <StaticRouter>
-      <OverviewCard module={module} />
-    </StaticRouter>,
-  );
+describe('Overview Card', () => {
+  test('Overview Card should show a course name', async () => {
+    const module = {
+      name: 'Test Title',
+    };
+    const overviewCard = render(
+      <StaticRouter>
+        <OverviewCard module={module} />
+      </StaticRouter>,
+    );
 
-  // console.log('overviewCard', overviewCard);
+    const title = await overviewCard.findByTestId('title-test');
+    expect(title.textContent).toMatch('Test Title');
+  });
 
-  const title = await overviewCard.findByTestId('title-test');
-  // console.log(title);
-  expect(title.textContent).toMatch('Test Title');
+  test('<Overview Card /> renders without crashing', () => {
+    const module = {
+      name: 'Coding with Friends',
+      moduleCode: 'CF123',
+      desc: 'Coding with my friends!',
+      progress: 0,
+      contents: [
+        {
+          number: 1,
+          title: 'Interactive Quiz',
+          desc: 'Asking students to vote on something',
+          completed: false,
+          notionContent: 'www.notion.com',
+          createdAt: '2021-09-30T13:49:51.721Z',
+          _id: { $oid: '6155c07fbaad8f31f4d9caa5' },
+        },
+      ],
+      createdAt: '2021-09-30T13:46:33.090Z',
+    };
+
+    const div = document.createElement('div');
+    render(<OverviewCard module={module} />, div);
+  });
 });
