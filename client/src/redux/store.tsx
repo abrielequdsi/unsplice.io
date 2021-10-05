67c7c4594f23b33481bea5eba6c26fb41a5c803d
Mary Wells <m.a.wells@outlook.com>
2021-10-05 11:45:18 +0100

    refactor: store to TS

--- client/src/redux/store.tsx
+++ client/src/redux/store.tsx
@@ -1,11 +1,6 @@
 import { createStore, compose } from 'redux';
 import reducers from './reducers/index.reducer';
 
-// const store = createStore(
-//   reducers,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-// );
-
 declare global {
   interface Window {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
